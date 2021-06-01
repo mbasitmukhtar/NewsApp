ecf43db8a8622e220796161dc8d238b010da6ba2
Muhammad Basit <basitmukhtar210@gmail.com>
2021-06-01 11:20:45 +0500

    initial commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "fd2af4ea8c9548f4ba43aaaf589ada0d"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
